2badbc7c5b879ff65cfdee38bb6695b5ec14fc44
Drawnskii <154537752+Drawnskii@users.noreply.github.com>
2024-01-15 21:20:38 -0500

    Se quedan solo los archivos necesarios. Se agregó una fuente y colores al tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        "light-purple": "#6642B1",
+        "dark-purple": "#2E1D4B",
+        "sky-blue": "#72E3FF",
+        "light-green": "#5CE8B1",
+      },
+    },
   },
   plugins: [],
-}
-
+};
